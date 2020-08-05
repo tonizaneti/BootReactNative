29b38e7f2ea79dc0a59cc7f9d37aee5e9b01ba61
tonizaneti <antonio.zaneti@gmail.com>
2020-08-05 12:35:13 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from "@react-navigation/native";
+import NewShowScreen from "./src/screens/NewShow";
+import ShowsScreen from "./src/screens/Shows";
+import HomeScreen from "./src/screens/Home";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
+import { AntDesign } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const TabNav = createBottomTabNavigator();
+const TabBarstyles  = StyleSheet.create({
+  viewStyle:{
+  flexDirection:"row",
+  justifyContent: "space-around"
+  }
+
+})
+
+const TabBar = ({navigation, state})=>{
+  
+  return(
+    <View style={TabBarStyles.viewStyle}>
+      <>
+        <TouchableOpacity onPress={()=>navigation.navigate("Home")}>
+          <AntDesign name="home" size={30} 
+          color={state.index===0?"blue":"black"} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>navigation.navigate("Shows")} >
+          <Entypo name="folder-video" size={30} 
+          color={state.index===1?"blue":"black"} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>navigation.navigate("NewShow")} >
+          <Entypo name="video" size={30} 
+          color={state.index===2?"blue":"black"} />
+        </TouchableOpacity>
+      </>
     </View>
-  );
+  )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App(){
+  return(
+    <NavigationContainer>
+      <TabNav.Navigator TabBar={props=><TabBar {...props}/>}>
+        <TabNav.Screen name={"Home"} component={HomeScreen} />
+        <TabNav.Screen name={"Shows"} component={ShowsScreen} />
+        <TabNav.Screen name={"NewShow"} component={NewShowScreen} />
+      </TabNav.Navigator>
+    </NavigationContainer>
+   );
+}
+
+export default App;
